9ce89754ebf8024bbc1323b4b16de4560c056ed5
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2023-04-06 18:38:35 +0100

    Simple script.

---
+++ Beginner/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# set the STRING variable
+STRING="Hello World!"
+# print the contents of the variable on screen
+echo $STRING#!/bin/bash
+# set the STRING variable
+STRING="Hello World!"
+# print the contents of the variable on screen
+echo $STRING#!/bin/bash
+# set the STRING variable
+STRING="Hello World!"
+# print the contents of the variable on screen
+echo $STRING#!/bin/bash
+# set the STRING variable
+STRING="Hello World!"
+# print the contents of the variable on screen
+echo $STRING
+
