8a9c8c03156760face015a529d652824b46eec90
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2023-04-06 18:20:39 +0100

    Create ThePythonCoderScanner.py

---
+++ Beginner/Basic Network Scanner/ThePythonCoderScanner.py
@@ -0,0 +1,26 @@
+from scapy.all import ARP, Ether, srp
+
+target_ip = "192.168.1.1/24" #From 0 to 255.
+# IP Address for the destination
+# create ARP packet
+arp = ARP(pdst=target_ip)
+# create the Ether broadcast packet
+# ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
+ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+# stack them
+packet = ether/arp
+
+result = srp(packet, timeout=3)[0] #Send packets at the DATA LINK layer (layer 2).
+
+# a list of clients, we will fill this in the upcoming loop
+clients = []
+
+for sent, received in result:
+    # for each response, append ip and mac address to `clients` list
+    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+
+# print clients
+print("Available devices in the network:")
+print("IP" + " "*18+"MAC")
+for client in clients:
+    print("{:16}    {}".format(client['ip'], client['mac']))
